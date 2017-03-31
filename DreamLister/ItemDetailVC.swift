b68efa999468d9bc613e6c55c9cc7e4de85120ba
Leonardo Correa <leonardo.correa@providesolution.com.br>
2017-03-31 18:35:24 -0300

    Implement item detail

---
+++ DreamLister/ItemDetailVC.swift
@@ -0,0 +1,38 @@
+//
+//  ItemDetailVC.swift
+//  DreamLister
+//
+//  Created by Leonardo Correa on 3/29/17.
+//  Copyright © 2017 providesolution. All rights reserved.
+//
+
+import UIKit
+
+class ItemDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let topItem = self.navigationController?.navigationBar.topItem {
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
+            
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
