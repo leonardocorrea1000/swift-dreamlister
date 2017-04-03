61fb246af342dd1e344bc916d75a64031427b15d
Leonardo Correa <leonardo.correa@providesolution.com.br>
2017-04-03 18:07:35 -0300

    Item Detail: Implement layout and picker

--- DreamLister/ItemDetailVC.swift
+++ DreamLister/ItemDetailVC.swift
@@ -7,8 +7,18 @@
 //
 
 import UIKit
+import CoreData
 
-class ItemDetailVC: UIViewController {
+class ItemDetailVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    @IBOutlet weak var storePicker: UIPickerView!
+    @IBOutlet weak var titleField: CustomTextField!
+    @IBOutlet weak var priceField: CustomTextField!
+    @IBOutlet weak var detailsField: CustomTextField!
+    
+    
+    var stores = [Store]();
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,22 +27,61 @@ class ItemDetailVC: UIViewController {
             topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
             
         }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        storePicker.delegate = self;
+        storePicker.dataSource = self;
+        
+//        let store = Store(context: context)
+//        store.name = "Best Buy";
+//        
+//        let store2 = Store(context: context)
+//        store2.name = "Telsa Dealership";
+//        let store3 = Store(context: context)
+//        store3.name = "Frys Electronics";
+//        let store4 = Store(context: context)
+//        store4.name = "Wallmart";
+//        let store5 = Store(context: context)
+//        store5.name = "Amazon";
+//        let store6 = Store(context: context)
+//        store6.name = "K Mart";
+//        
+//        ad.saveContext()
+        
+        getStores()
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        let store = stores[row];
+        return store.name;
     }
-    */
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return stores.count
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // Update UI
+    }
+    
+    func getStores() {
+        let fetchRequest: NSFetchRequest<Store> = Store.fetchRequest()
+        
+        do {
+            self.stores = try context.fetch(fetchRequest);
+            self.storePicker.reloadAllComponents();
+        } catch {
+            
+            // handle the error
+            
+        }
+    }
+
+
 
 }
